02e5f42a191a9c74f695072c0eeaa8df98641bc1
lavenderlyj <498926778@qq.com>
2015-10-05 00:52:39 +0800

    第二次作业

--- web/index.dart
+++ web/index.dart
@@ -2,28 +2,21 @@ import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = '='
+    ..onClick.listen(addNumber);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+void addNumber(MouseEvent event)
+{ var i;
+  var A= int.parse(document.getElementById("left").value);
+  var B=int.parse(document.getElementById("right").value);
+  var sum=0;
+  var C;
+  if(A>B)
+  { C=A;
+    A=B;
+    B=C;}
+  for(i=A;i<=B;i++)
+  sum=sum+i;
+querySelector('#sample_text').text=sum.toString();
 }
