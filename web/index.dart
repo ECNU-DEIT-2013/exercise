b157d61c4226c43130414d32873d7b0a9d26616b
nadja2000 <lingzi200@icloud.com>
2015-10-06 11:27:40 +0800

    1+2+3+4+...100=5050

--- web/index.dart
+++ web/index.dart
@@ -1,37 +1,22 @@
 import 'dart:html';
 void main() {
   querySelector('#add').onClick.listen(add);
-  querySelector('#cut').onClick.listen(cut);
-  querySelector('#ride').onClick.listen(ride);
-  querySelector('#divide').onClick.listen(divide);
+
 
 }
 
 void add(MouseEvent event){
-  int x =int.parse(document.getElementById("x").value);
-  int y =int.parse(document.getElementById("y").value);
-  var z =x+y;
-  querySelector('#z').value=z.toString();
-}
-void cut(MouseEvent event){
-  int x =int.parse(document.getElementById("x").value);
-  int y =int.parse(document.getElementById("y").value);
-  var z =x-y;
+
+  int z=0;
+  for(int i=1;i<=100;i++)
+    {z +=i;
+    }
   querySelector('#z').value=z.toString();
 }
 
-void ride(MouseEvent event){
-  int x =int.parse(document.getElementById("x").value);
-  int y =int.parse(document.getElementById("y").value);
-  var z =x*y;
-  querySelector('#z').value=z.toString();
-}
-void divide(MouseEvent event){
-  int x =int.parse(document.getElementById("x").value);
-  int y =int.parse(document.getElementById("y").value);
-  var z =x/y;
-  querySelector('#z').value=z.toString();
-}
+
+
+
 
 
 
