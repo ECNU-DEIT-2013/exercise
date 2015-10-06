33709f8d6f8968b0a103ee3a51a7dd4428b6d8f2
nadja2000 <lingzi200@icloud.com>
2015-10-06 10:17:00 +0800

    可以进行加减乘除四则运算

--- web/index.dart
+++ web/index.dart
@@ -1,29 +1,39 @@
 import 'dart:html';
-import 'dart:math' as math;
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+  querySelector('#add').onClick.listen(add);
+  querySelector('#cut').onClick.listen(cut);
+  querySelector('#ride').onClick.listen(ride);
+  querySelector('#divide').onClick.listen(divide);
+
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+void add(MouseEvent event){
+  int x =int.parse(document.getElementById("x").value);
+  int y =int.parse(document.getElementById("y").value);
+  var z =x+y;
+  querySelector('#z').value=z.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+void cut(MouseEvent event){
+  int x =int.parse(document.getElementById("x").value);
+  int y =int.parse(document.getElementById("y").value);
+  var z =x-y;
+  querySelector('#z').value=z.toString();
 }
+
+void ride(MouseEvent event){
+  int x =int.parse(document.getElementById("x").value);
+  int y =int.parse(document.getElementById("y").value);
+  var z =x*y;
+  querySelector('#z').value=z.toString();
+}
+void divide(MouseEvent event){
+  int x =int.parse(document.getElementById("x").value);
+  int y =int.parse(document.getElementById("y").value);
+  var z =x/y;
+  querySelector('#z').value=z.toString();
+}
+
+
+
+
+
