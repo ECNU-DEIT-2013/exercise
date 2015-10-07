c4a974be18a1a1ee9f8bc35da26fcfc879e3b00a
zwbbbb <765041490@qq.com>
2015-10-07 17:16:41 +0800

    作业2 1加到100

--- web/index.dart
+++ web/index.dart
@@ -1,29 +1,17 @@
 import 'dart:html';
-import 'dart:math' as math;
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+  querySelector('#equal').onClick.listen(add);
+
+
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+void add(MouseEvent event){
+
+  int z=0;
+  for(int i=1;i<=100;i++)
+  {z +=i;
   }
-  querySelector('#sample_text_id').text = buffer.toString();
+  querySelector('#z').value=z.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
-}
+
+
