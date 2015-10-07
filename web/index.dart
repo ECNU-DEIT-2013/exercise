7b61e8578d5e6bcb9264571039b8aa89f3a6f757
litingtingann <2460695697@qq.com>
2015-10-07 14:41:31 +0800

    01
    李婷婷的第一次作业

--- web/index.dart
+++ web/index.dart
@@ -2,28 +2,15 @@ import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = '1+2+3+4...+100='
+    ..onClick.listen(Add);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+void Add(MouseEvent event){
+  var i=0;
+  var s=0;
+  for(i=1;i<=100;i++)
+  {s=s+i;}
+  querySelector('#sample_text_id').text=s.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
-}
+
