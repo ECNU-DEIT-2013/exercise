6e42c1793ff6accddfe5bc26a1a253e79f7e1015
刘傲然 <823219343@qq.com>
2015-10-07 15:58:15 +0800

    Revert "10130340206_1"
    This reverts commit cd9ccfd82dc52b958f2dbc89d40a5da9068c8d55.

--- web/index.dart
+++ web/index.dart
@@ -2,8 +2,8 @@ import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-    ..text = '1 added to 100 equals'
-    ..onClick.listen(adds);
+    ..text = 'Click me!'
+    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
 }
 
 void reverseText(MouseEvent event) {
@@ -14,31 +14,16 @@ void reverseText(MouseEvent event) {
   }
   querySelector('#sample_text_id').text = buffer.toString();
 }
-
-
-
-void randomSelectStu(MouseEvent event) {
-  var stuMap = {
-    0:1213344345,
-    1:6786876872
+void randomStudentID(String showStr,MouseEvent even){
+  var students={
+    0:10001,
+    1:10002,
+    2:100003,
+    3:100004,
+    4:100005,
+    5:100005
   };
   var random = new math.Random();
-
-  var randomID = random.nextInt(2);
-  var stuID = stuMap[randomID];
-  querySelector('#sample_studentid_id').text = stuID.toString();
-}
-
-  void add(MouseEvent event) {
-    var number1 = 3;
-    var number2 = 6;
-    var number3 = number1 + number2;
-    querySelector('#sample_sum_id').text = number3.toString();
-  }
-
-void adds(MouseEvent event) {
-  var s=0;
-  for(var i=1;i<=100;i++)
-  {s=s+i;}
-  querySelector('#sample_sums_id').text =s.toString();
+  var getYou=students[random.nextInt(6)];
+  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
 }
