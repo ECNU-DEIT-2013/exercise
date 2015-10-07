73246d9943b08727e8a5e3a09d2e00e3542e46dd
fanyunying <1129917237@qq.com>
2015-10-07 23:39:59 +0800

    exercis_1

--- web/index.dart
+++ web/index.dart
@@ -4,6 +4,13 @@ void main() {
   querySelector('#sample_text_id')
     ..text = 'Click me!'
     ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+  querySelector('#sample_text_id')
+  ..text="Please input the number."
+  ..onClick.listen(myText);
+  querySelector('#sample_text_result')
+  ..text="Click me to get the result"
+  ..onClick.listen(add);
+
 }
 
 void reverseText(MouseEvent event) {
@@ -13,6 +20,14 @@ void reverseText(MouseEvent event) {
     buffer.write(text[i]);
   }
   querySelector('#sample_text_id').text = buffer.toString();
+}
+
+void myText(MouseEvent event) {
+  var a = document.getElementById("myTexta").value;
+  var b = document.getElementById("myTextb").value;
+  var A = int.parse(a);
+  var B = int.parse(b);
+  addNumber(A, B);
 }
 void randomStudentID(String showStr,MouseEvent even){
   var students={
@@ -26,4 +41,18 @@ void randomStudentID(String showStr,MouseEvent even){
   var random = new math.Random();
   var getYou=students[random.nextInt(6)];
   querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+}
+
+void addNumber(num a,num b){
+  var c=a+b;
+  document.getElementById("sample_studentid_id").value=c.toString();
+}
+
+void add(MouseEvent event){
+  var s=0;
+  for(int i=1; i<101; i++)
+  {
+    s=s+i;
+  }
+  querySelector('#sample_text_result').text=s.toString();
 }
