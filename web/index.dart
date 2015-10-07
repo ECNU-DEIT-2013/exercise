72e7cc8a0ae79411fe216eb319966077da1879c8
张莞雪 <595480373@qq.com>
2015-10-07 15:36:23 +0800

    exercise-1
    从1加到100，学生张莞雪

--- web/index.dart
+++ web/index.dart
@@ -2,8 +2,10 @@ import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = 'add 1 to 100:'
+    ..onClick.listen(add);
+
+
 }
 
 void reverseText(MouseEvent event) {
@@ -14,16 +16,24 @@ void reverseText(MouseEvent event) {
   }
   querySelector('#sample_text_id').text = buffer.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
+
+
+
+void plus1(MouseEvent event) {
+  var stuMap={
+    0:1213344345,
+    1:6786876872
   };
   var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+
+  var randomID=random.nextInt(2);
+  var stuID=stuMap[randomID];
+  querySelector('#sample_studentid_id').text =stuID.toString();
+}
+void add(MouseEvent event){
+  int number=0;
+  for(int i=1; i<=100;i++)
+  { number=number + i;}
+  querySelector('#sample_text_id').text = number.toString();
+
 }
