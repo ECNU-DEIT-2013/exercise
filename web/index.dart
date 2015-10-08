778aab8cbe3c923404c8e935fc6a0a7ee2bb3159
mifang <10130340227@ecnu.cn>
2015-10-08 18:49:01 +0800

    Revert "exercise_1"
    This reverts commit b21ddcdcf9786bdc8fbebdfd1f1f8a0df3c3ff7b.

--- web/index.dart
+++ web/index.dart
@@ -1,20 +1,30 @@
 import 'dart:html';
 import 'dart:math' as math;
-
 void main() {
   querySelector('#sample_text_id')
     ..text = 'Click me!'
-    ..onClick.listen(add);
-  querySelector('#sample_sum_id')
-    ..text = 'the sum of all the numbers from 1 to 10';
-
-
+    ..onClick.listen(randomSelectStu);
 }
 
-void add(MouseEvent event) {
-  int s=0;
-  for(int i=1;i<=10;i++){
-    s=s+i;
+void reverseText(MouseEvent event) {
+  var text = querySelector('#sample_text_id').text;
+  var buffer = new StringBuffer();
+  for (int i = text.length - 1; i >= 0; i--) {
+    buffer.write(text[i]);
   }
-  querySelector('#text1').value = s.toString();
+  querySelector('#sample_text_id').text = buffer.toString();
+}
+
+
+
+void randomSelectStu(MouseEvent event) {
+  var stuMap={
+    0:1213344345,
+    1:6786876872
+  };
+  var random = new math.Random();
+
+  var randomID=random.nextInt(2);
+  var stuID=stuMap[randomID];
+  querySelector('#sample_studentid_id').text =stuID.toString();
 }
