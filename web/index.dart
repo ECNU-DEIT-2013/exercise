420132ebe1dcd7e0bc1e8448e7ead0df7190ef8e
余玲菲 <1272479098@qq.com>
2015-10-09 01:45:58 +0800

    1113034201

--- web/index.dart
+++ web/index.dart
@@ -3,31 +3,14 @@ import 'dart:math' as math;
 import 'utility/people.dart';
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = '1+2+3+...+100='
+    ..onClick.listen(add);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+void add(MouseEvent event) {
+  int x=0;
+  for (int i =  1; i <= 100; i++) {
+    x += i;
   }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var ple= new people();
-ple..lastName='aa'
-  ..firstName='bb';
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = ple.lastName;
+  querySelector('#x').text = x.toString();
 }
