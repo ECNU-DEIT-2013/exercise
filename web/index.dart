e02d90b023c6ba1242e33ea10a9e70b88b794295
代维 <1362039936@qq.com>
2015-10-08 16:57:13 +0800

    1

--- web/index.dart
+++ web/index.dart
@@ -3,9 +3,13 @@ import 'dart:math' as math;
 
 void main() {
  querySelector('#button_for_add')
-    ..text='='
+    ..text='+'
     ..onClick.listen(addUpNumber);
   querySelector('#sample_text_id2').text=addUp(0,0).toString();
+ querySelector('#button_for_add1')
+   ..text='-'
+   ..onClick.listen(addUpNumber1);
+ querySelector('#sample_text_id2').text=addUp(0,0).toString();
 }
 
 void addUpNumber (MouseEvent event){
@@ -13,8 +17,17 @@ void addUpNumber (MouseEvent event){
   var y = int.parse(querySelector('#input2').value);
   querySelector('#sample_text_id2').text=addUp(x,y).toString();
 }
+void addUpNumber1 (MouseEvent event){
+  var x = int.parse(querySelector('#input1').value);
+  var y = int.parse(querySelector('#input2').value);
+  querySelector('#sample_text_id2').text=addUp1(x,y).toString();
+}
 
 int addUp(var x,var y){
-  var z=x+y;
-  return z;
+  var m=x+y;
+  return m;
+}
+int addUp1(var x,var y){
+  var n=x-y;
+  return n;
 }
