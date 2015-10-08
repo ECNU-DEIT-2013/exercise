8e1e4d19b12485f1d69fd2a977a8696da4d1377d
田彬 <1280039028@qq.com>
2015-10-08 19:13:02 +0800

    exercise_1修改

--- web/index.dart
+++ web/index.dart
@@ -9,9 +9,8 @@ void main() {
 
 void add(MouseEvent event){
   var sum=0;
-  for (var i = 0; i < 100; i++) {
+  for (var i = 1; i <= 100; i++) {
     sum=sum+i;
   }
-  print('1+2+3+бн+100=');
   querySelector('#sample_teat_id').text = sum.toString();
 }
