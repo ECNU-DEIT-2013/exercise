a88dc90ae13015bebd5e305f9726d10e1257b397
zhao13 <574959780@qq.com>
2015-10-09 17:17:27 +0800

    exercise_1
    add 2 nums/ add from 1 to 100

--- web/index.dart
+++ web/index.dart
@@ -1,33 +1,43 @@
 import 'dart:html';
 import 'dart:math' as math;
-import 'utility/people.dart';
+
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text='Click me!'
+    ..onClick.listen(randomSelectStu);
+  querySelector('#button_for_add')
+    ..text='Add them Up'
+    ..onClick.listen(addThemUp);
+  querySelector('#button1')
+  ..text='1+2+3+...+100='
+  ..onClick.listen(LianJia);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+void LianJia (MouseEvent event){
+  var num = 0;
+  for (var i=1;i<=100;i++)
+    num=num+i;
+  querySelector('#Result').text=num.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
+
+void addThemUp (MouseEvent event){
+  var x = int.parse(querySelector('#input1').value);
+  var y = int.parse(querySelector('#input2').value);
+  querySelector('#sample_text_id2').text=addUp(x,y).toString();
+}
+
+int addUp(var x,var y){
+  var z=x+y;
+  return z;
+}
+
+void randomSelectStu(MouseEvent event){
+  var stuMap = {
+    0:'1ºÅ',
+    1:'2ºÅ',
+    2:'3ºÅ'  };
   var random = new math.Random();
-  var ple= new people();
-ple..lastName='aa'
-  ..firstName='bb';
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = ple.lastName;
+  var randomID = random.nextInt(3);
+  var stuName = stuMap[randomID];
+  querySelector('#sample_student_id').text=stuName;
 }
