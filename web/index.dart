250f954ab7e9218868f343bfc0c618d8c420e74a
bosswan <1849115675@qq.com>
2015-10-09 16:08:41 +0800

    10130340105

--- web/index.dart
+++ web/index.dart
@@ -1,42 +1,39 @@
+import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-   // ..text = 'Click me!'
-  //  ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e))
-..text = '='
-..onClick.listen(addNumber);
+    ..text = 'Add Number'
+    ..onClick.listen(randomSelectStu);
+   // ..onClick.listen(reverseText);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+//void reverseText(MouseEvent event) {
+ // var text = querySelector('#sample_text_id').text;
+ // var buffer = new StringBuffer();
+  //for (int i = text.length - 1; i >= 0; i--) {
+  //  buffer.write(text[i]);
+ // }
+  //querySelector('#sample_text_id').text = buffer.toString();
+//}
+
+
+
+void randomSelectStu(MouseEvent event) {
+  var stuMap={
+    0:"eg: 5+6=11",
+    1:"eg: 7+9=16"
+  };
+  var random = new math.Random();
+  var randomID=random.nextInt(2);
+  var stuID=stuMap[randomID];
+  querySelector('#sample_studentid_id').text =stuID.toString();
 }
-//void randomStudentID(String showStr,MouseEvent even){
- // var students={
-  //  0:100,
-  //  1:200,
-  //  2:123,
-   // 3:125,
-   // 4:155,
-   // 5:195
-//  };
-//  var random = new math.Random();
-//  var getYou=students[random.nextInt(6)];
-//  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;}
-  void addNumber(MouseEvent event)
-  { var i;
-  var A= int.parse(document.getElementById("left").value);
-  var B=int.parse(document.getElementById("right").value);
-  var sum=0;
-  var C;
-  if(A>B)
-  { C=A;
-  A=B;
-  B=C;}
-  for(i=A;i<=B;i++)
-    sum=sum+i;
-  querySelector('#sample_text').text=sum.toString();}
+
+//void AddNumber(num a,b){
+ // var text = querySelector('#sample_text_id').text;
+  //var buffer = new StringBuffer();
+ // text[i]=5+6
+    //buffer.write(text[i]);
+
+//};
+
