fc326179fc599ce3397e1b877044899f3c183340
YJZHOU <yjzhouecnu@163.com>
2015-10-10 00:13:46 +0800

    exercise_1 改进
    用dart添加和删除加法器中的输入框和按钮。

--- web/index.dart
+++ web/index.dart
@@ -1,16 +1,47 @@
 import 'dart:html';
 import 'dart:math' as math;
+InputElement mytexta;
+InputElement mytextb;
+UListElement sample_container_id;
 void main() {
 
-   querySelector('#sample_text_id')
-    ..text="Input The Numbers First AND Click Me To Get The Result"
-    ..onClick.listen(myText);
+ querySelector('#sample_text_id')
+   ..text="Input The Numbers First AND Click Me To Get The Result"
+   ..onClick.listen(myText);
    querySelector('#sample_text_result')
      ..text="CLICK ME TO GET THE RESULT"
      ..onClick.listen(addhundred);
+  querySelector('#sample_text_result2')
+    ..text="Input The Numbers First AND Click Me To Get The Result"
+    ..onClick.listen(addnum);
+
+  mytexta=querySelector('#myTexta');
+  mytextb=querySelector('#myTextb');
+ sample_container_id=querySelector('#sample_container_id');
+ mytextb.onChange.listen(addnumnew);
 }
 
+void addnumnew(Event e){
+  var a =  document.getElementById("myTexta").value;
+  var b = document.getElementById("myTextb").value;
+  var A = int.parse(a);
+  var B = int.parse(b);
+  int c=A+B;
+  var resultc=new InputElement();
+  resultc.value=c.toString();
+  sample_container_id.children.add(resultc);
+  var button1=new ButtonInputElement();
+  button1.value="CLEAR";
+  sample_container_id.children.add(button1);
+  button1.onClick.listen(mybutton);
+  button1.onClick.listen((e)=> button1.remove());
+  button1.onClick.listen((e)=> resultc.remove());
+}
+void mybutton(MouseEvent event){
+  document.getElementById("myTexta").value="Input A Number";
+  document.getElementById("myTextb").value="Input A Number";
 
+}
 void myText(MouseEvent event){
   var a =  document.getElementById("myTexta").value;
   var b = document.getElementById("myTextb").value;
@@ -22,6 +53,8 @@ void myText(MouseEvent event){
 
 void addNumber(num a,num b){
   var c=a+b;
+
+  resultc.value=c.toString();
   document.getElementById("sample_studentid_id").value=c.toString();
 }
 
@@ -31,4 +64,17 @@ void addhundred(MouseEvent event){
   s=s+i;
   }
   querySelector('#sample_text_result').text=s.toString();
+
+
+}
+void addnum(MouseEvent event){
+  var n =  document.getElementById("myTextx").value;
+  var N = int.parse(n);
+  var s=0;
+  for (int i = 1; i <N+1; i++){
+    s=s+i;
+  }
+  querySelector('#sample_text_result2').text=s.toString();
+
+
 }
