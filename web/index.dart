44670527a6f7afcbcdaf107cb7c0756f0d642f29
TingtingCao <1259346056@qq.com>
2015-10-09 09:38:03 +0800

    10130340220

--- web/index.dart
+++ web/index.dart
@@ -1,33 +1,39 @@
 import 'dart:html';
 import 'dart:math' as math;
-import 'utility/people.dart';
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = 'Add Number'
+    ..onClick.listen(randomSelectStu);
+   // ..onClick.listen(reverseText);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
+//void reverseText(MouseEvent event) {
+ // var text = querySelector('#sample_text_id').text;
+ // var buffer = new StringBuffer();
+  //for (int i = text.length - 1; i >= 0; i--) {
+  //  buffer.write(text[i]);
+ // }
+  //querySelector('#sample_text_id').text = buffer.toString();
+//}
+
+
+
+void randomSelectStu(MouseEvent event) {
+  var stuMap={
+    0:"eg: 5+6=11",
+    1:"eg: 7+9=16"
   };
   var random = new math.Random();
-  var ple= new people();
-ple..lastName='aa'
-  ..firstName='bb';
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = ple.lastName;
+  var randomID=random.nextInt(2);
+  var stuID=stuMap[randomID];
+  querySelector('#sample_studentid_id').text =stuID.toString();
 }
+
+//void AddNumber(num a,b){
+ // var text = querySelector('#sample_text_id').text;
+  //var buffer = new StringBuffer();
+ // text[i]=5+6
+    //buffer.write(text[i]);
+
+//};
+
