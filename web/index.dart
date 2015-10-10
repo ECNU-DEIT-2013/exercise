b766542dbf820bdff2174cac2647508cc072f00c
megumii <854613989@qq.com>
2015-10-10 20:36:24 +0800

    exercise_2

--- web/index.dart
+++ web/index.dart
@@ -1,15 +1,37 @@
 import 'dart:html';
 import 'dart:math' as math ;
 
+InputElement toDoInput;
+UListElement toDoList;
+ButtonElement deleteAll;
+
 void main() {
+
   querySelector('#sample_text_id')
     ..text = '1+2+...+100='
     ..onClick.listen(oneHundred);
 
   querySelector('#button_add_id')
     ..text='Add'
-    ..onClick.listen(addThemUp);
+    ..onClick.listen(addToDoItem);
+
+  deleteAll = querySelector('#delete-all');
+  deleteAll.onClick.listen((e) => toDoList.children.clear());
+
+  toDoList = querySelector('#to-do-list');
+  toDoInput.onChange.listen(addToDoItem);
 }
+
+void addToDoItem(Event e) {
+  var x = int.parse(querySelector('#number_a').value);
+  var y = int.parse(querySelector('#number_b').value);
+  var z = x+y;
+  var newToDo = new LIElement();
+  newToDo.text = z.toString();
+  newToDo.onClick.listen((e) => newToDo.remove());
+  toDoList.children.add(newToDo);
+}
+
 
 int addDo(var a, var b){
   var c = a+b;
