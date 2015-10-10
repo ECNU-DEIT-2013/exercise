e6ff64a7b23acbf64bdee273459b050cdefc638f
NJLZWP <1546754993@qq.com>
2015-10-10 14:44:04 +0800

    problem1

--- web/index.dart
+++ web/index.dart
@@ -1,16 +1,17 @@
 import 'dart:html';
 import 'dart:math' as math;
+
+UListElement toDoList;
+
 void main() {
-  querySelector('#sample_text_id')
-    ..text = '1+2+3+...+98+99+100=?'
-    ..onClick.listen(add);
+  querySelector('#to-do-input')
+    ..onClick.listen(addToDoItem);
 }
 
 
-void add(MouseEvent event) {
-  int i=0;
-  int m=0;
-  for (i=1;i<=100;i++)
-  {m=m+i;}
-  querySelector('#sample_text_id').text = m.toString();
+void addToDoItem(Event e) {
+  var newToDo=new LIElement();
+  newToDo.value="1234";
+  toDoList.children.add(newToDo);
+
 }
