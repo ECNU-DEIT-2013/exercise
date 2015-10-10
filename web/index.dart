fe278b70b11bf22983c72a902d2ede88c4257438
张莞雪 <595480373@qq.com>
2015-10-11 00:52:38 +0800

    exercise-2(2)
    最后实现加法

--- web/index.dart
+++ web/index.dart
@@ -1,48 +1,35 @@
 import 'dart:html';
 import 'dart:math' as math;
-InputElement toDoButton;
+InputElement toDoInput1;
+InputElement toDoInput2;
 UListElement toDoList;
-void main() {
-
-  toDoButton = querySelector('#to-do-button');
-  toDoList = querySelector('#to-do-list');
-  toDoButton.onClick.listen(addToDoItem);
-
+ButtonElement Button1;
+ButtonElement Button2;
+void main(){
+  toDoInput1=querySelector('#input1');
+  toDoInput2=querySelector('#input3');
+  toDoList=querySelector('#list_id');
+  Button1=querySelector('#button1');
+  Button2=querySelector('#button2');
+  Button1.onClick.listen((e) {
+    toDoList.children.clear();
+  });
+  Button1.onClick.listen(delete);
+  Button2.onClick.listen(AddInput);
 }
-void addToDoItem(Event e) {
-  var newToDo = new TextInputElement();
-  newToDo.text = '';
+
+void AddInput(Event e){
+  var newToDo = new InputElement();
+  var one,two,three,a,b;
+  one= toDoInput1.value;
+  two=toDoInput2.value;
+  a=int.parse(one);
+  b=int.parse(two);
+  three=a+b;
+  newToDo.value=three.toString();
   toDoList.children.add(newToDo);
 }
-
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-
-
-
-void plus1(MouseEvent event) {
-  var stuMap={
-    0:1213344345,
-    1:6786876872
-  };
-  var random = new math.Random();
-
-  var randomID=random.nextInt(2);
-  var stuID=stuMap[randomID];
-  querySelector('#sample_studentid_id').text =stuID.toString();
-}
-void add(MouseEvent event){
-  var number=0;
-  for (int i = toDoList.text.length - 1; i >= 0; i--){
-    toDoList.text=toDoList.toString();
-     number=number+toDoList.text[i];
-   }
-    querySelector('#sample_text_id').text = number.toString();
-
+void delete(Event e){
+  toDoInput1.value='';
+  toDoInput2.value='';
 }
