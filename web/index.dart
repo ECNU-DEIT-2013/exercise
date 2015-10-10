f88a68be7becf75778cc739d45f5bbf7e0765916
田彬 <1280039028@qq.com>
2015-10-10 23:39:03 +0800

    exercise_1修改2

--- web/index.dart
+++ web/index.dart
@@ -1,16 +1,30 @@
 import 'dart:html';
 
+var number=0;
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen(add);
+
+  querySelector('#sample_text_id1')
+    ..text = '1+..+100=?'
+    ..onClick.listen(addSum);
+  querySelector('#sample_text_id2')
+    ..text = 'Here is the answer...';
+  querySelector('#button').onClick.listen(add);
 }
-
-
 void add(MouseEvent event){
-  var sum=0;
-  for (var i = 1; i <= 100; i++) {
-    sum=sum+i;
-  }
-  querySelector('#sample_teat_id').text = sum.toString();
+  int a =int.parse(document.getElementById("text1").value);
+  int b =int.parse(document.getElementById("text2").value);
+  var x =a+b;
+  querySelector('#button').value=x.toString();
 }
+
+void addSum(MouseEvent event) {
+  var number=0;
+
+  for (var i = 1;i<=100;i++)
+  {
+    number = i+ number;
+  }
+
+  querySelector('#sample_text_id2').text = number.toString();
+}
+
