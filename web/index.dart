59efc3d50d2d86a6c2fbf455751e034cbe452579
fanyunying <1129917237@qq.com>
2015-10-11 18:15:18 +0800

    exercise2

--- web/index.dart
+++ web/index.dart
@@ -1,16 +1,32 @@
 import 'dart:html';
 import 'dart:math' as math;
+InputElement toDoInput, toDoInput1, toDoInput2;
+UListElement toDoList;
+
 void main() {
+  toDoInput=querySelector('#to-do-input');
+  toDoInput1=querySelector('#to-do-input1');
+  toDoInput2=querySelector('#to-do-input2');
+  toDoList=querySelector('#to-do-list');
+
   querySelector('#sample_text_id')
     ..text = 'Click me!'
     ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
-  querySelector('#sample_text_id')
-  ..text="Please input the number."
-  ..onClick.listen(myText);
-  querySelector('#sample_text_result')
-  ..text="Click me to get the result"
-  ..onClick.listen(add);
+  querySelector('#sample_text_id2')
+    ..text="Please input the number.";
+  querySelector('#sample_text_id3')
+    ..text="Click me to get the result";
+    querySelector('#button').onClick.listen(add);
+    querySelector('#button').onClick.listen(addToDoItem);
+}
 
+void addToDoItem(Event e) {
+
+  var newToDo = new LIElement();
+  newToDo.text =toDoInput1.value+"+"+toDoInput2.value+ "="+toDoInput.value;
+  querySelector('#button').value = 'answer';
+
+  toDoList.children.add(newToDo);
 }
 
 void reverseText(MouseEvent event) {
@@ -22,13 +38,14 @@ void reverseText(MouseEvent event) {
   querySelector('#sample_text_id').text = buffer.toString();
 }
 
-void myText(MouseEvent event) {
-  var a = document.getElementById("myTexta").value;
-  var b = document.getElementById("myTextb").value;
-  var A = int.parse(a);
-  var B = int.parse(b);
-  addNumber(A, B);
+void add(MouseEvent event){
+  int a =int.parse(document.getElementById("to-do-input1").value);
+  int b =int.parse(document.getElementById("to-do-input2").value);
+  var x =a+b;
+  querySelector('#button').value=x.toString();
+  toDoInput.value =x.toString();
 }
+
 void randomStudentID(String showStr,MouseEvent even){
   var students={
     0:10001,
@@ -41,18 +58,4 @@ void randomStudentID(String showStr,MouseEvent even){
   var random = new math.Random();
   var getYou=students[random.nextInt(6)];
   querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
-}
-
-void addNumber(num a,num b){
-  var c=a+b;
-  document.getElementById("sample_studentid_id").value=c.toString();
-}
-
-void add(MouseEvent event){
-  var s=0;
-  for(int i=1; i<101; i++)
-  {
-    s=s+i;
-  }
-  querySelector('#sample_text_result').text=s.toString();
 }
