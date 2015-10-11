723821ed47d3e09f9fa3f60d59a4868b36a4a422
weiqiaoru <734288799@qq.com>
2015-10-11 21:12:26 +0800

    exercise_1
    第一次作业 加法器 （传了很多次了 不知道出了什么问题= =  老师见谅）

--- web/index.dart
+++ web/index.dart
@@ -1,33 +1,19 @@
 import 'dart:html';
 import 'dart:math' as math;
-import 'utility/people.dart';
+
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+  querySelector('#button_for_add')
+    ..text='+'
+    ..onClick.listen(addNumber);
+  querySelector('#sample_text_id2').text=addUp(0,0).toString();
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+void addNumber (MouseEvent event){
+  var x = int.parse(querySelector('#input1').value);
+  var y = int.parse(querySelector('#input2').value);
+  querySelector('#sample_text_id2').text=add(x,y).toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var ple= new people();
-ple..lastName='aa'
-  ..firstName='bb';
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = ple.lastName;
+int add(var x,var y){
+  var m=x+y;
+  return m;
 }
