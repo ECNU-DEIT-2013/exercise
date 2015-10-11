6c69fce01d9e531ad41b108c8e8c6ae09447399e
litingtingann <2460695697@qq.com>
2015-10-11 10:43:26 +0800

    smallchange

--- web/index.dart
+++ web/index.dart
@@ -1,9 +1,10 @@
 import 'dart:html';
 import 'dart:math' as math;
 void main() {
-  querySelector('#sample_text_id')
-    ..text = '1+2+3+4...+100='
+  querySelector('#btn1')
     ..onClick.listen(Add);
+  querySelector('#btn2')
+    ..onClick.listen(add);
 }
 
  Add(MouseEvent event){
@@ -11,6 +12,13 @@ void main() {
   var s=0;
   for(i=1;i<=100;i++)
   {s=s+i;}
-  querySelector('#sample_text_id').text=s.toString();
+  querySelector('#txt1').value=s.toString();
 }
 
+add(MouseEvent event){
+  var a=0;
+  var b=0;
+  var c=0;
+  c=a+b;
+  querySelector('#txt1').value=s.toString();
+}
