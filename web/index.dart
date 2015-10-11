106a8d8c3c241c9f6c58eb8b28829aaac520363a
侯嫣茹 <740408368@qq.com>
2015-10-11 10:18:13 +0800

    exercise_2
    用dart 编程控制html文档中控件的动态添加和删除，以此完善加法

--- web/index.dart
+++ web/index.dart
@@ -1,9 +1,39 @@
 import 'dart:html';
 import 'dart:math' as math;
+
+InputElement toDoInput;
+UListElement toDoList;
+ButtonElement toDoButton,toDobutton2,toDobutton3;
+
 void main() {
-  querySelector('#sample_text_id')
-    ..text = '1+2+3+...+100='
-    ..onClick.listen(add);
+  toDoButton = querySelector('#button');
+  toDobutton2 = querySelector('#button2');
+  toDoList = querySelector('#to-do-list');
+  toDoButton.onClick.listen(addToDoItem);
+  toDobutton2.onClick.listen(adding);
+  querySelector('#button3').onClick.listen((e) => toDoList.children.clear());
+  querySelector('#sample_text_id').onClick.listen(add);
+}
+
+void remove(Event e){
+  toDoList.children[0].text='';
+  toDoList.children[1].text='';
+}
+
+void adding(Event e){
+  var a,b,x,y,z;
+  a=toDoList.children[0].value;
+  b=toDoList.children[1].value;
+  x=int.parse(a);
+  y=int.parse(b);
+  z=x+y;
+  querySelector('#result').text=z.toString();
+}
+
+void addToDoItem(Event e) {
+  var newInput = new TextInputElement();
+  newInput.value = '';
+  toDoList.children.add(newInput);
 }
 
 void add(MouseEvent event) {
