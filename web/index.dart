30b8d91f6b3f75bf016364f062d8c48ef8acb0d0
代维 <1362039936@qq.com>
2015-10-11 17:53:05 +0800

    exercise_2
    进阶的加法器

--- web/index.dart
+++ web/index.dart
@@ -1,20 +1,41 @@
 import 'dart:html';
 import 'dart:math' as math;
-
+ButtonElement DeleteAll;
+ButtonElement QingKong;
 void main() {
- querySelector('#button_for_add')
-    ..text='='
+  querySelector('#button_for_add')
+    ..text='+'
     ..onClick.listen(addUpNumber);
-  querySelector('#sample_text_id2').text=addUp(0,0).toString();
+  querySelector('#button_for_add1')
+    ..text='-'
+    ..onClick.listen(addUpNumber1);
+  QingKong=querySelector('#sample_qingkong_id');
+  QingKong.onClick.listen(Qingkong);
+}
+
+void Qingkong(Event e){
+  querySelector('#input1').value='';
+  querySelector('#input2').value='';
+  querySelector('#sample_input1_id').value='';
 }
 
 void addUpNumber (MouseEvent event){
   var x = int.parse(querySelector('#input1').value);
   var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text=addUp(x,y).toString();
+  querySelector('#sample_input1_id').value=addUp(x,y).toString();
+}
+
+void addUpNumber1 (MouseEvent event){
+  var x = int.parse(querySelector('#input1').value);
+  var y = int.parse(querySelector('#input2').value);
+  querySelector('#sample_input1_id').value=addUp1(x,y).toString();
 }
 
 int addUp(var x,var y){
-  var z=x+y;
-  return z;
+  var m=x+y;
+  return m;
+}
+int addUp1(var x,var y){
+  var n=x-y;
+  return n;
 }
