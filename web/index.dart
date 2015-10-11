1e46eee7f38a094ba3f6f99e5c8ba9d88e0a9a8b
TingtingCao <1259346056@qq.com>
2015-10-11 17:43:32 +0800

    exercise_2
    加法器改良版！

--- web/index.dart
+++ web/index.dart
@@ -1,39 +1,48 @@
 import 'dart:html';
 import 'dart:math' as math;
-void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Add Number'
-    ..onClick.listen(randomSelectStu);
-   // ..onClick.listen(reverseText);
+InputElement myInput;
+UListElement myList;
+ButtonElement DeleteAll;
+ButtonElement QingKong;
+void main(){
+  myInput=querySelector('#sample_input_id');
+  myList=querySelector('#sample_todolist_id');
+  DeleteAll=querySelector('#sample_button_id');
+  DeleteAll.onClick.listen((e) {
+    myList.children.clear();
+  });
+  QingKong=querySelector('#sample_denghao_id');
+  QingKong.onClick.listen(Qingkong);
+  myInput.onChange.listen(AddInput);
 }
-
-//void reverseText(MouseEvent event) {
- // var text = querySelector('#sample_text_id').text;
- // var buffer = new StringBuffer();
-  //for (int i = text.length - 1; i >= 0; i--) {
-  //  buffer.write(text[i]);
- // }
-  //querySelector('#sample_text_id').text = buffer.toString();
-//}
-
-
-
-void randomSelectStu(MouseEvent event) {
-  var stuMap={
-    0:"eg: 5+6=11",
-    1:"eg: 7+9=16"
-  };
-  var random = new math.Random();
-  var randomID=random.nextInt(2);
-  var stuID=stuMap[randomID];
-  querySelector('#sample_studentid_id').text =stuID.toString();
+void randomSelectStu(String c, MouseEvent event){
+  var A,B;
+  A=document.getElementById('sample_numberone_id').value;
+  B=document.getElementById('sample_numbertwo_id').value;
+  var a=int.parse(A);
+  var b=int.parse(B);
+  c=a+b;
+  document.getElementById("sample_studentid_id").value=c.toString();
 }
-
-//void AddNumber(num a,b){
- // var text = querySelector('#sample_text_id').text;
-  //var buffer = new StringBuffer();
- // text[i]=5+6
-    //buffer.write(text[i]);
-
-//};
-
+void AddNumber(int s, MouseEvent event){
+  int i;
+  for(i=1;i<=100;i++)
+    s=s+i;
+  document.getElementById("sample_add_id").value=s.toString();
+}
+void AddInput(Event e){
+  var newToDo=new InputElement();
+  var A,B,a,b,c;
+  A=document.getElementById('sample_input1_id').value;
+  B=myInput.value;
+  a=int.parse(A);
+  b=int.parse(B);
+  c=a+b;
+  newToDo.value=c.toString();
+  newToDo.onClick.listen((e)=>newToDo.remove());
+  myList.children.add(newToDo);
+}
+void Qingkong(Event e){
+  myInput.value='';
+  querySelector('#sample_input1_id').value='';
+}
