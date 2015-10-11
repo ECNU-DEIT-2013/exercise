0efe8ed4b3cc186663cbbc7694651b1db65bf169
lmf <10130340227@ecnu.cn>
2015-10-11 12:51:07 +0800

    exercise_2
    加法的完善

--- web/index.dart
+++ web/index.dart
@@ -1,30 +1,35 @@
 import 'dart:html';
-import 'dart:math' as math;
+
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen(randomSelectStu);
+  querySelector('#sample_ask_id')
+    ..text = 'Please input A and B then click one as your requirement';
+  querySelector('#sum').onClick.listen((MouseEvent event) => sum(int.parse(document.getElementById("text1").value),int.parse(document.getElementById("text2").value),event));
+  querySelector('#mul').onClick.listen((MouseEvent event) => multiply(int.parse(document.getElementById("text1").value),int.parse(document.getElementById("text2").value),event));
+  querySelector('#enter').onClick.listen(clean);
 }
-
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+void clean(MouseEvent event){
+  querySelector('#text1').value = null;
+  querySelector('#text2').value = null;
+  querySelector('#text3').value = null;
+  querySelector('#sign1').text = '';
+  querySelector('#sign2').text = '';
+}
+void sum(int n1,n2,MouseEvent event){
+  querySelector('#sign1').text = 'sum';
+  querySelector('#sign2').text = '';
+  int s=0;
+  for(int i = n1; i <= n2; i++){
+    s=s+i;
   }
-  querySelector('#sample_text_id').text = buffer.toString();
+  querySelector('#text3').value = s.toString();
 }
 
-
-
-void randomSelectStu(MouseEvent event) {
-  var stuMap={
-    0:1213344345,
-    1:6786876872
-  };
-  var random = new math.Random();
-
-  var randomID=random.nextInt(2);
-  var stuID=stuMap[randomID];
-  querySelector('#sample_studentid_id').text =stuID.toString();
+void multiply(int n1,n2, MouseEvent event) {
+  querySelector('#sign1').text = '';
+  querySelector('#sign2').text = 'multiply';
+  int s = 1;
+  for (int i = n1; i <= n2; i++) {
+    s = s * i;
+  }
+  querySelector('#text3').value = s.toString();
 }
