fb1a6a40c494a1fe1b490aec1ffc38008611b2e5
litingtingann <2460695697@qq.com>
2015-10-11 15:59:35 +0800

    exercise_2
    第二次作业初稿

--- web/index.dart
+++ web/index.dart
@@ -1,24 +1,35 @@
 import 'dart:html';
 import 'dart:math' as math;
 void main() {
-  querySelector('#btn1')
-    ..onClick.listen(Add);
-  querySelector('#btn2')
-    ..onClick.listen(add);
+ querySelector('#btn1')
+    ..onClick.listen((MouseEvent e)=>add1(int.parse(querySelector('#txt1').value),int.parse(querySelector('#txt2').value),e));
+ querySelector('#btn2')
+   ..onClick.listen((MouseEvent e)=>add2(int.parse(querySelector('#txt1').value),int.parse(querySelector('#txt2').value),e));
+  querySelector('#btn3')
+    ..onClick.listen(clear);
 }
 
- Add(MouseEvent event){
-  var i;
+void add1(int m,int n, MouseEvent e){
   var s=0;
-  for(i=1;i<=100;i++)
+ if(m>n){
+    var temp;
+    temp=m;
+    m=n;
+    n=temp;
+  }
+  for(int i=m;i<=n;i++)
   {s=s+i;}
-  querySelector('#txt1').value=s.toString();
+  querySelector('#txt3').value=s.toString();
 }
 
-add(MouseEvent event){
-  var a=0;
-  var b=0;
+void add2(int m,int n, MouseEvent e){
   var c=0;
-  c=a+b;
-  querySelector('#txt1').value=s.toString();
+  c=m+n;
+  querySelector('#txt3').value=c.toString();
 }
+
+clear(MouseEvent event){
+  querySelector('#txt1').value='';
+  querySelector('#txt2').value='';
+  querySelector('#txt3').value='';
+  }
