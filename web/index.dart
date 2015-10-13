c7c30b0c1b07ddd08baa8ba206f49e27bf819f07
miaoxiaole <11156216446@qq.com>
2015-10-13 14:41:46 +0800

    exercise-2
    exexcise

--- web/index.dart
+++ web/index.dart
@@ -1,13 +1,19 @@
 import 'dart:html';
+import 'dart:math' as math;    //11
+Element new1,new2;
 InputElement t1,t2,t3,t4;
 UListElement ul1;
+List<ButtonElement> buttons =new List();
 int i;
+Map map1 = { 1:'a', 2:'b', 0:'c '};     //11
 void main() {
   i=1;
   ul1 =querySelector('#hello');
   t1=querySelector('#text1');
   t2=querySelector('#text2');
   t3=querySelector('#text3');
+  new1=querySelector('#new1');
+  new2=querySelector('#new2');
   querySelector('#button')
     ..onClick.listen((MouseEvent e)=>start(int.parse(t1.value),int.parse(t2.value),e))
     ..onClick.listen(start2);
@@ -16,6 +22,7 @@ void main() {
     ..onClick.listen(start1)
     ..onClick.listen(start2);
   querySelector('#button3').onClick.listen(start3);
+  newLetters();
 }
 String start(int origin1,int origin2,MouseEvent e){
   if(origin1>origin2){
@@ -76,4 +83,25 @@ String sum1(){
   }
   return he;
 }
-
+void moveLetters(MouseEvent e){
+  Element letter2 =e.target;
+  if(letter2.parent==new1){
+    new2.children.add(letter2);
+  }
+  else
+  {
+    new1.children.add(letter2);
+  }
+}
+newLetters(){
+  var random = new math.Random();
+  buttons.clear();
+  for (var j = 0; j < 3; j++) {
+    int letter1 = random.nextInt(3);
+    buttons.add(new ButtonElement());   //在button列表中添加一个新的button元素；
+    buttons[j].classes.add("letter");    // 给这个button的类中添加一个名为“letter”的类别
+    buttons[j].onClick.listen(moveLetters);
+    buttons[j].text = map1[letter1];        //这个按钮里面的字母是从字符串中取来的；
+    new1.children.add(buttons[j]);                 //在letterPile这个块里面添加上这一个按钮；
+  }
+}
