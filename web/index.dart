fc07c95bc0c19f843e0a14dd9bbb6b041e356969
bestwishes369 <673800843@qq.com>
2015-10-13 13:06:46 +0800

    Revert "exercise_1"
    This reverts commit f74e7200f1a83498a58e4a10c90da4306bf81edf.
    
    Conflicts:
    	web/index.css
    	web/index.dart
    	web/index.html

--- web/index.dart
+++ web/index.dart
@@ -1,13 +1,23 @@
 import 'dart:html';
-import 'dart:math' as math;
+int a=1;
+UListElement toDoList=querySelector('#toDoList');
+UListElement toDoList2=querySelector('#toDoList2');
+ButtonElement deleteAll;
 void main() {
-  querySelector('#botton1_id').onClick.listen((MouseEvent e)=>add1to100(int.parse(document.getElementById("text1").value),int.parse(document.getElementById("text2").value),e));
-  querySelector('#sample_text_id').text = 'Click me!';
-  querySelector('#botton2_id')
-  ..onClick.listen(addToDoItem);
+  //querySelector('#botton1_id').onClick.listen((MouseEvent e)=>add1to100(int.parse(querySelector('#text1').value),int.parse(querySelector('#text2').value),e));
+  querySelector('#botton2_id')//Ìí¼ÓÔªËØ
+  ..onClick.listen(addToDoItem)
+  ..onClick.listen(addLi)
+  ..onClick.listen(addToDoItem2);
+
+  deleteAll = querySelector('#delete-all');//É¾³ýÔªËØ
+  deleteAll
+  ..onClick.listen((e) => toDoList.children.clear())
+  ..onClick.listen((e) => toDoList2.children.clear());
 }
 
 void add1to100(int origin1,int origin2,MouseEvent e) {
+  //querySelector('#sample_studentid_id').text="win";
   int sum=0;
   for (int i = origin1; i <=origin2; i++) {
     sum+=i;
@@ -17,8 +27,32 @@ void add1to100(int origin1,int origin2,MouseEvent e) {
 }
 void addToDoItem(Event e)
 {
-  UListElement toDoList=querySelector('#toDoList');
-  var newToDo=new TextInputElement();
-  newToDo.value="12334";
+ if(a<=2){
+   var newToDo=new TextInputElement();
+   newToDo.placeholder="the first number";
+   toDoList.children.add(newToDo);
+   a++;
+ }
+}
+void addToDoItem2(Event e)
+{
+  if(a<=2){
+    var newToDo=new TextInputElement();
+    var newButton=new ButtonElement();
+    newToDo.placeholder="the second number";
+    toDoList2.children.add(newToDo);
+    newButton.text="START";
+    newButton.onClick.listen((MouseEvent e)=>add1to100(1,100,e));
+    toDoList2.children.add(newButton);
+    a++;
+  }
+}
+
+void addLi(Event e)
+  {
+  LIElement toDoList=querySelector('#toDoLi');
+  var newToDo=new LIElement();
+  newToDo.text="clicked";
+  newToDo.onClick.listen((MouseEvent e)=>newToDo.remove());
   toDoList.children.add(newToDo);
 }
