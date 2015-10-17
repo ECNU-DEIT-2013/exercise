e6f038d36be122953b6a7c78726dae639dedfcc8
meflyup <meflyup@gmail.com>
2015-10-17 11:24:10 +0800

    无法计算
    无法计算，请查看dart文件中的注释说明

--- web/dart.dart
+++ web/dart.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 /**
  * 无法运行，请调试排查
+ * 另外，每次作业提交时，commit中请用exercise-x 来提交，x表示1，2，3，类推。一定要填写commit消息，方便自己以方便其他代码阅读者看
+* 参考地址：https://meflyup.gitbooks.io/dartgate/content/course7.html
  */
 void main()
 {
