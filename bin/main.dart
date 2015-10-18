1748993867301720a57f901dd1c19062a3a9f486
zwbbbb <765041490@qq.com>
2015-10-18 19:27:57 +0800

    作业三

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,27 @@
 import 'dart:io';
+import 'dart:convert' show UTF8, JSON;
 
 main() async {
+  List jsondata = [
+    "portmanteau", "fantabulous", "spork", "smog",
+    "spanglish", "gerrymander", "turducken", "stagflation",
+    "bromance", "freeware", "oxbridge", "palimony", "netiquette",
+    "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
+  ];
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
-
-}
-
-
   await for (var request in server) {
-    request.response
-      ..headers.contentType = new ContentType("text", "html", charset: "utf-8")
-      ..write('<p>portmanteau<p>')
+    HttpResponse res = request.response;
+    addCorsHeaders(res);
+    res
+      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+      ..write(JSON.encode(jsondata))
       ..close();
   }
 }
-processString(String jsonString) {
-  List<String> portmanteaux = JSON.decode(jsonString);
-  for (int i = 0; i < portmanteaux.length; i++) {
-    wordList.children.add(new LIElement()..text = portmanteaux[i]);
-  }
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
 
-handleError(Object error) {
-  wordList.children.add(new LIElement()..text = 'Request failed.');
-}
