2ca59722a3471c48b2ac6525d6990c9efd7e84ca
MingyueCui <yueyueyiyiyou@hotmail.com>
2015-10-18 23:25:42 +0800

    exercise_3

--- web/index.dart
+++ web/index.dart
@@ -1,24 +1,68 @@
+// Copyright (c) 2012, the Dart project authors.  
+// Please see the AUTHORS file for details. 
+// All rights reserved. Use of this source code 
+// is governed by a BSD-style license that can be 
+// found in the LICENSE file.
+
+/*import 'dart:async';
+import 'dart:convert';
 import 'dart:html';
 
-//////////////////////
-InputElement todo_input;
-UListElement todo_list;
-ButtonElement delet_eall;
-///////////////////////
-void main() {
-  todo_input=querySelector('#todoinput');
-  todo_list=querySelector('#todolist');
-  delet_eall=querySelector('#deleteall');
-  delet_eall.onClick.listen((e)=>todo_list.children.clear());
-todo_list.onClick.listen((e)=>todo_list.firstChild.remove());
+var wordList;
 
-  todo_input.onChange.listen(addItem);
+void main() {
+  querySelector('#getWords').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
 }
 
-void addItem(Event e){
-  var newtodo=new LIElement();
-  newtodo.text=todo_input.value;
-  todo_input.value='';
-  todo_list.children.add(newtodo);
+Future makeRequest(Event e) async {
+  var path = 'http://127.0.0.1:8091';
+  try {
+    processString(await HttpRequest.getString(path));
+  } catch (e) {
+    print('Couldn\'t open $path');
+    handleError(e);
+  }
+}
 
+processString(String jsonString) {
+if (request.status == 200) {
+List<String> portmanteaux = JSON.decode(request.responseText);
+for (int i = 0; i < portmanteaux.length; i++) {
+wordList.children.add(new LIElement()..text = portmanteaux[i]);
+}
+} else {
+wordList.children.add(new LIElement()
+..text = 'Request failed, status=${request.status}');
+}
+}*/
+import 'dart:html';
+import 'dart:convert';
+
+var wordList;
+
+void main() {
+  querySelector('#getWords').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
+}
+
+void makeRequest(Event e) {
+  var path = 'http://127.0.0.1:8091';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
+}
+
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text = portmanteaux[i]);
+    }
+  } else {
+    wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
+  }
 }
