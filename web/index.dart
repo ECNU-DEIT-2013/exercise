4d788637d2be6bb859f1a56bae5f950adbb3de3e
weiqiaoru <734288799@qq.com>
2015-10-18 15:57:09 +0800

    exercise_3
    sever好像调用不成功

--- web/index.dart
+++ web/index.dart
@@ -1,61 +1,31 @@
 import 'dart:html';
-import 'dart:math' as math;
+import 'dart:convert';
+
+var wordList;
 
 void main() {
-  querySelector('#button_for_add')
-    ..text='+'
-    ..onClick.listen(addUpNumber);
-  querySelector('#sample_text_id2').text=addUp(0,0).toString();
-
-  querySelector('#button_for_sub')
-    ..text='-'
-    ..onClick.listen(SubNumber);
-  querySelector('#sample_text_id2').text=sub(0,0).toString();
-
-  querySelector('#button_for_mul')
-    ..text='*'
-    ..onClick.listen(MulNumber);
-  querySelector('#sample_text_id2').text=mul(0,0).toString();
-
-  querySelector('#button_for_div')
-    ..text='/'
-    ..onClick.listen(DivNumber);
-  querySelector('#sample_text_id2').text=div(0,0).toString();
+  querySelector('#getWords').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
 }
 
-void addUpNumber (MouseEvent event) {
-  var x = int.parse(querySelector('#input1').value);
-  var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text = addUp(x, y).toString();
+void makeRequest(Event e) {
+  var path = 'http://127.0.0.1:1122';
+  var httpRequest = new HttpRequest();
+
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
 }
-void SubNumber (MouseEvent event){
-  var x = int.parse(querySelector('#input1').value);
-  var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text=sub(x,y).toString();
-}
-void MulNumber (MouseEvent event){
-  var x = int.parse(querySelector('#input1').value);
-  var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text=mul(x,y).toString();
-}
-void DivNumber (MouseEvent event) {
-  var x = int.parse(querySelector('#input1').value);
-  var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text = div(x, y).toString();
-}
-int addUp(var x,var y){
-  var m=x+y;
-  return m;
-}
-int sub(var x,var y){
-  var n=x-y;
-  return n;
-}
-int mul(var x,var y){
-  var o=x*y;
-  return o;
-}
-int div(var x,var y) {
-  var p = x /= y;
-  return p;
+
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text = portmanteaux[i]);
+    }
+  } else {
+    wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
+  }
 }
