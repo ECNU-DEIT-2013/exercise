ce3642586e5516977f31f5925126c52e926460a0
zwbbbb <765041490@qq.com>
2015-10-18 19:37:08 +0800

    第三次作业

--- web/index.dart
+++ web/index.dart
@@ -1,71 +1,29 @@
 import 'dart:html';
-import 'dart:math' as math;
-InputElement toDoInput;
-UListElement toDoList;
-UListElement list1;
-ButtonElement toDoButton;
-ButtonElement addup;
-ButtonElement addthemup;
+import 'dart:convert';
 
+var wordList;
 
-int sum=0;
-int j=0;
- void main() {
-querySelector('#sample_text_id1');
-
-
-  toDoButton = querySelector('#to-do-button');
-  toDoButton.onClick.listen(addToDoItem1);
-
-
-
-  addup = querySelector('#button1');
-  list1 = querySelector('#list1');
-  addup.onClick.listen(addnumup);
-
+void main() {
+    querySelector('#getWords').onClick.listen(makeRequest);
+    wordList = querySelector('#wordList');
 
 }
-
-void addnumberup(Event e){
-  var a=toDoList.children[0].value;
-  var b=toDoList.children[1].value;
-  var x=int.parse(a);
-  var y=int.parse(b);
-  var c=x+y;
-  var newToDo=new TextInputElement();
-  newToDo.value=c.toString();
-  newToDo.onClick.listen((e)=>newToDo.remove());
-  list3.children.add(newToDo);
+void makeRequest(Event e) {
+  var path = 'http://127.0.0.1:8080/';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
 }
-
-void addnumup(Event e){
-  var a=int.parse(querySelector('#kongge').value);
- var b=int.parse(querySelector('#kongge1').value);
-  var c=a+b;
-  var newToDo=new TextInputElement();
-  newToDo.value=c.toString();
-  newToDo.onClick.listen((e)=>newToDo.remove());
-  list1.children.add(newToDo);
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text= portmanteaux[i]);
+    }
+  } else {
+   wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
+  }
 }
-
-
-
-
-
-void addToDoItem1(Event e) {
-  var newToDo=new TextInputElement();
-  newToDo.text='';
-  toDoList.children.add(newToDo);
-}
-
-
-
- void display(MouseEvent event){
-j=1;
-}
-
-
-
-
-
-
