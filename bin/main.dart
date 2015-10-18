d69118fb9a6bc5eb48b62181512a80a42f2eca11
mumu0803 <mumuqaz@qq.com>
2015-10-18 16:43:04 +0800

    exercise_3

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,7 @@ main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
 
+
   await for (var request in server) {
     request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
