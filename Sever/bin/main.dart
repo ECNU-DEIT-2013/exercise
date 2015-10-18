b992108636170640b293088d8dd6dbff136ecfc4
田彬 <1280039028@qq.com>
2015-10-18 19:09:58 +0800

    exercise_3
    第三次作业：构建自己的服务器

---
+++ Sever/bin/main.dart
@@ -0,0 +1,29 @@
+
+//main(List<String> args) {
+ // print('Hello world!');
+//}
+import 'dart:io';
+
+main() async {
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+    HttpResponse res=request.response;
+    addCorsHeaders(res);
+    request.response
+      ..headers.contentType = new ContentType("appliciation", "json", charset:"utf-8")
+      ..write('''["portmanteau","fantabulous","spork,smog","spanglish","gerrymander","turducken","stagflation","bromance",
+      "freeware","oxbridge","palimony","netiquett","brunch","blog","chortle","Hassenpfeffer","Schnitzelbank"]''')
+      ..close();
+  }
+void addCorsHeaders(HttpRespose res){
+  res.headers.add("Access-Control-Allow-Origin","*");
+  res.headers.add("Access-Control-Allow-Methods","POST,GET,OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+}
+}
+
+
+
+
--- web/index.dart
+++ web/index.dart
@@ -1,45 +1,30 @@
 import 'dart:html';
-InputElement toDoInput,toDoInput1,toDoInput2;
-UListElement toDoList;
+import 'dart:convert';
 
-//var number=0;
+var wordList;
+
 void main() {
-  toDoInput = querySelector('#to-do-input');
-  toDoInput1 = querySelector('#to-do-input1');
-  toDoInput2 = querySelector('#to-do-input2');
-  toDoList = querySelector('#to-do-list');
-
-  querySelector('#sample_text_id1')
-    ..text = '1+..+100=?'
-    ..onClick.listen(addSum);
-  querySelector('#sample_text_id2')
-    ..text = 'Here is the answer...';
-  querySelector('#button').onClick.listen(add);
-  querySelector('#button').onClick.listen(addToDo);
-}
-void addToDo(Event e) {
-
-  var newToDo = new LIElement();
-  newToDo.text =toDoInput1.value+" + "+toDoInput2.value+ " = "+toDoInput.value;
-  querySelector('#button').value = 'answer';
-
-  toDoList.children.add(newToDo);
-}
-void add(MouseEvent event){
-  int a =int.parse(document.getElementById("to-do-input1").value);
-  int b =int.parse(document.getElementById("to-do-input2").value);
-  var x =a+b;
-  querySelector('#button').value =x.toString();
-  toDoInput.value =x.toString();
+  querySelector('#getWords').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
 }
 
-void addSum(MouseEvent event) {
-  var number=0;
-  for (var i = 1;i<=100;i++)
-  {
-    number = i+ number;
+void makeRequest(Event e) {
+  var path = 'http://127.0.0.1:8080';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
+}
+
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text = portmanteaux[i]);
+    }
+  } else {
+    wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
   }
-  querySelector('#sample_text_id2').text = number.toString();
 }
-
-
