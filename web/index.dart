e2ee98839f5f1ed824c27b845f821c4f4a5a4f34
zhao13 <574959780@qq.com>
2015-10-18 16:55:06 +0800

    exercise_2

--- web/index.dart
+++ web/index.dart
@@ -1,43 +1,25 @@
 import 'dart:html';
 import 'dart:math' as math;
 
-void main() {
-  querySelector('#sample_text_id')
-    ..text='Click me!'
-    ..onClick.listen(randomSelectStu);
-  querySelector('#button_for_add')
-    ..text='Add them Up'
-    ..onClick.listen(addThemUp);
-  querySelector('#button1')
-  ..text='1+2+3+...+100='
-  ..onClick.listen(LianJia);
+InputElement toDoInput;
+UListElement toDolist;
+
+void main()
+{
+  toDoInput=querySelector('#sample_todoinput_id');
+  toDolist=querySelector('#sample_todolist_id');
+  toDoInput.onChange.listen(addThemUp);
+
+
 }
 
-void LianJia (MouseEvent event){
-  var num = 0;
-  for (var i=1;i<=100;i++)
-    num=num+i;
-  querySelector('#Result').text=num.toString();
-}
-
-void addThemUp (MouseEvent event){
-  var x = int.parse(querySelector('#input1').value);
-  var y = int.parse(querySelector('#input2').value);
-  querySelector('#sample_text_id2').text=addUp(x,y).toString();
-}
-
-int addUp(var x,var y){
+void addThemUp(Event e){
+  var newToDo=new LIElement();
+  var x = int.parse(querySelector('#sample_todoinput1_id').value);
+  var y = int.parse(querySelector('#sample_todoinput_id').value);
   var z=x+y;
-  return z;
+  newToDo.text=z.toString();
+  toDoList.children.add(newToDo);
 }
 
-void randomSelectStu(MouseEvent event){
-  var stuMap = {
-    0:'1ºÅ',
-    1:'2ºÅ',
-    2:'3ºÅ'  };
-  var random = new math.Random();
-  var randomID = random.nextInt(3);
-  var stuName = stuMap[randomID];
-  querySelector('#sample_student_id').text=stuName;
-}
+
