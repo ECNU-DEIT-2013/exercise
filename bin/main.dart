d08d555b0696ffacde4557f40243573c59ea1fe6
lisulin <1126361165@qq.com>
2015-10-18 21:36:51 +0800

    Exercise_3
    bin文件夹中建立的8080服务器
    web文件夹中请求网址可以返回字符串

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 main() async {
@@ -6,9 +5,17 @@ main() async {
   print("Serving at ${server.address}:${server.port}");
 
   await for (var request in server) {
+    var res= request.response;
+    addCorsHeaders(res);
     request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
       ..write('[\n "portmanteau", "fantabulous", "spork", "smog",\n "spanglish", "gerrymander", "turducken", "stagflation",\n "bromance", "freeware", "oxbridge", "palimony", "netiquette",\n "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"\n]')
       ..close();
   }
+}
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add('Access-Control-Allow-Origin', '*');
+  res.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.headers.add('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
 }
