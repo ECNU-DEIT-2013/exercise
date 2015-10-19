e3d1ada7c2c82776508b8ae71f456594b7f79614
meflyup <meflyup@gmail.com>
2015-10-19 08:48:06 +0800

    请查看代码错误
    两处代码错误导致不能运行。你可以在github中查看对比信息

--- bin/main.dart
+++ bin/main.dart
@@ -9,7 +9,7 @@ main() async {
     ..add("Access-Control-Allow-Origin", "*")
     ..add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
     ..add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    resquest.response
+    request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
       ..write('[\n "portmanteau", "fantabulous", "spork", "smog",\n "spanglish", "gerrymander", "turducken", "stagflation",\n "bromance", "freeware", "oxbridge", "palimony", "netiquette",\n "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"\n]\n')
       ..close();
--- web/main.dart
+++ web/main.dart
@@ -15,7 +15,7 @@ void main() {
 }
 
 void makeRequest(Event e) {
-  var path = 'https://127.0.0.1:8080/';
+  var path = 'http://127.0.0.1:8080';
   var httpRequest = new HttpRequest();
   httpRequest
     ..open('GET', path)
