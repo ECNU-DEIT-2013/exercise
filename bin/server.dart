d6a26366fb95470d4d279cd41c0a2b6859d9e8cb
jelly01304 <416866867@qq.com>
2015-10-20 00:13:54 +0800

    建立服务器
    参照案例，自己建立服务器，调试成功。
    P.S.:周末电脑出现一些状况，未能及时提交作业，抱歉~

---
+++ bin/server.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+
+main() async {
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+    HttpResponse res = request.response;
+    addCorsHeaders(res);
+    request.response
+      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+      ..write('''[
+  "portmanteau", "fantabulous", "spork", "smog",\n"spanglish", "gerrymander", "turducken", "stagflation",\n"bromance", "freeware", "oxbridge", "palimony", "netiquette",\n"brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
+]'''
+    )
+      ..close();
+  }
+}
+
--- web/index.dart
+++ web/index.dart
@@ -1,69 +1,35 @@
+// Copyright (c) 2012, the Dart project authors.
+// Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:html';
-import 'dart:math' as math;
-InputElement toDoInput,toDoInput1,toDoInput2;
-UListElement toDoList;
+import 'dart:convert';
+var wordList;
 
-var number=0;
-void main() {
-  toDoInput = querySelector('#to-do-input');
-  toDoInput1 = querySelector('#to-do-input1');
-  toDoInput2 = querySelector('#to-do-input2');
-  toDoList = querySelector('#to-do-list');
-
-  querySelector('#sample_text_id')
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
-  querySelector('#sample_text_id2')
-    ..text = '1+..+100=?'
-    ..onClick.listen(addSum);
-  querySelector('#sample_text_id3')
-    ..text = 'Here is the answer...';
-  querySelector('#button').onClick.listen(add);
-  querySelector('#button').onClick.listen(addToDoItem);
+main() async{
+  querySelector('#getWords').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
 }
-void addToDoItem(Event e) {
 
-  var newToDo = new LIElement();
-  newToDo.text =toDoInput1.value+"+"+toDoInput2.value+ "="+toDoInput.value;
-  querySelector('#button').value = 'Start';
+void makeRequest(Event e) {
+  var path = 'http://127.0.0.1:8080';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
+}
 
-  toDoList.children.add(newToDo);
-}
-void add(MouseEvent event){
-  int a =int.parse(document.getElementById("to-do-input1").value);
-  int b =int.parse(document.getElementById("to-do-input2").value);
-  var x =a+b;
-  querySelector('#button').value =x.toString();
-  toDoInput.value =x.toString();
-}
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text = portmanteaux[i]);
+    }
+  } else {
+    wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
   }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-void addSum(MouseEvent event) {
-  var number=0;
-
-  for (var i = 1;i<=100;i++)
-  {
-    number = i+ number;
-  }
-
-  querySelector('#sample_text_id3').text = number.toString();
-}
-
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
 }
