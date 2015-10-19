3a8c62cfe66569324a871342d36ca4a25798d8f1
litingtingann <2460695697@qq.com>
2015-10-19 12:08:34 +0800

    exercise-3
    改进版

--- bin/myserver.dart
+++ bin/myserver.dart
@@ -6,6 +6,9 @@ main() async {
 
   await for (var request in server) {
     request.response
+    HttpResponse res = request.response;
+    addCorsHeaders(res);
+    res
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
       ..write(''''[
     "portmanteau", "fantabulous", "spork", "smog",
@@ -15,4 +18,9 @@ main() async {
     ] ''')
       ..close();
   }
+}
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
