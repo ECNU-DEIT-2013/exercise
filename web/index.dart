1a31fac97ef9244a957c30f2a6503c1edf994cc9
Kellyjiang <3167361419@qq.com>
2015-10-20 09:05:30 +0800

    Revert "exercise_2"
    This reverts commit 103a1deb910bbfdfbb9e71ee56e87e5e493a18e2.

--- web/index.dart
+++ web/index.dart
@@ -1,33 +1,15 @@
 import 'dart:html';
-DivElement Div;
-InputElement Input1,Input2;
-ButtonElement Result,Delete;
 void main()
 {
-  Div=querySelector('#add');
-  Input1=querySelector('#number1');
-  Input2=querySelector('#number2');
-  Result=querySelector('#btn1');
-  Delete=querySelector('#btn2');
-  Result.onClick.listen(Add);
-  Delete.onClick.listen((e)=>Div.children.clear());
-  Delete.onClick.listen(deleteall);
+  querySelector('#btn').onClick.listen(NumberAdd);
+
 }
-
-
-void Add(Event e)
-{
-  var newToDo=new InputElement();
-  var result;
-  result=int.parse(querySelector('#number1').value)+int.parse(querySelector('#number2').value);
-  newToDo.value=result.toString();
-  newToDo.onClick.listen((e)=>newToDo.remove());
-
-  Div.children.add(newToDo);
-}
-
-
-void deleteall(MouseEvent event) {
-  Input1.value = '';
-  Input2.value = '';
+void NumberAdd(MouseEvent event){
+  var result=0;
+  var m;
+  for(m=1;m<11;m++)
+  {
+    result+=m;
+  }
+  querySelector('#txt1').value=result.toString();
 }
