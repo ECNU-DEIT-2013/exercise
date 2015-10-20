8604467eca6a21a934796f78f039c04f0bf8c5f8
weiqiaoru <734288799@qq.com>
2015-10-20 08:55:35 +0800

    exercise_3.1
    讲解后程序改进

--- bin/sever.dart
+++ bin/sever.dart
@@ -6,9 +6,23 @@ main() async {
   print("Serving at ${server.address}:${server.port}");
 
   await for (var request in server) {
+    var response = request.response;
+    addCorsHeaders(response);
     request.response
-      ..headers.contentType = new ContentType("text", "josn", charset: "utf-8")
-      ..write('["portmanteau", "fantabulous", "spork", "smog","spanglish", "gerrymander", "turducken", "stagflation","bromance", "freeware", "oxbridge", "palimony", "netiquette","brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"]')
+      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+      ..write('''[
+    "portmanteau", "fantabulous", "spork", "smog",
+    "spanglish", "gerrymander", "turducken", "stagflation",
+    "bromance", "freeware", "oxbridge", "palimony", "netiquette",
+    "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
+    ]''')
       ..close();
   }
+}
+void addCorsHeaders(HttpResponse response) {
+  response.headers.add('Access-Control-Allow-Origin', '*');
+  response.headers.add(
+      'Access-Control-Allow-Methods', 'POST, OPTIONS');
+  response.headers.add('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
 }
