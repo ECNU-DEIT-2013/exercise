e9f63eadf86a7718e58da2af88e696f8190db965
bestwishes369 <673800843@qq.com>
2015-10-22 18:51:22 +0800

    DataBase connected
    实现数据库连接，数据库相关操作还需优化

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
-
+import 'package:sqljocky/sqljocky.dart';
 main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
+  connectDB();
 
   await for (var request in server) {
     HttpResponse res = request.response;
@@ -21,4 +22,12 @@ void addCorsHeaders(HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+connectDB() async{
+  var pool = new ConnectionPool(host: 'localhost', port: 3306, user: 'root', password: '1234', db: 'stu', max: 5);
+  var results = await pool.query('select * from stu1');
+  results.forEach((row) {
+    print('Name: ${row[0]}, email: ${row[1]}');
+  });
+
 }
