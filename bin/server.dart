568c8054df81123c72f947d4d4bb7f3d3d051b58
jelly01304 <416866867@qq.com>
2015-10-29 00:24:54 +0800

    exercise4
    能连接个人数据库，将stu表中的两个字段信息写入服务器传到客户端，替换原先write中的内容。
    用html,css调整了一些界面。

--- bin/server.dart
+++ bin/server.dart
@@ -1,25 +1,50 @@
 import 'dart:io';
+import 'package:sqljocky/sqljocky.dart';
+import 'dart:convert';
 
+List table=new List();int i=0;
 void addCorsHeaders(HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
 
+connect() async{
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340208', max: 10);
+  var results = await pool.query('select * from stu;');
+
+  results.forEach((row) {
+    print('stu_id: ${row[0]},stu_name: ${row[1]}');
+    table.add('stu_id:${row[0]}, stu_name:${row[1]}');
+    i++;
+    print(table[i-1]);
+  });
+}
+
 main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
-
+  connect();
   await for (var request in server) {
+    var list = JSON.encode(table);
     HttpResponse res = request.response;
     addCorsHeaders(res);
     request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write('''[
-  "portmanteau", "fantabulous", "spork", "smog",\n"spanglish", "gerrymander", "turducken", "stagflation",\n"bromance", "freeware", "oxbridge", "palimony", "netiquette",\n"brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
-]'''
+      ..write('${list}'
     )
       ..close();
+
+/*  res
+      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+      ..write('[');
+
+    for(int i=0;i<table.length;i++)
+      //res.write('"${table[i]}"');
+
+    res.write(']');
+    res.close();
+
+    */
   }
 }
-
