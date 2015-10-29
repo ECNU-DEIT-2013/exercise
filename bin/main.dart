2c271cc57f348c552d8d6209d436b6c55601cdd1
YJZHOU <yjzhouecnu@163.com>
2015-10-30 00:17:04 +0800

    exercise_4 dart连接服务器
    创建数据库，并用dart连接数据库读取数据，转换为json格式传给客户端。再用dart动态控制html的技术，将上述从服务器获得的json数据在页面上展现出来。（点击不同按钮，出现相应数据）
    仍存在几个问题，写在web\main.dart的备注里。

--- bin/main.dart
+++ bin/main.dart
@@ -1,22 +1,36 @@
 import 'dart:io';
+import 'package:sqljocky/sqljocky.dart';
+import 'dart:convert' show UTF8, JSON;
 
 main() async {
+  var userlist=new List();
+   //var IDlist = new List();
+   //var Namelist=new List();
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
-
   await for (var request in server) {
+    var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user:"dec2013stu", password: 'dec2013stu', db: 'stu_10130340210');
+    var results = await pool.query('select user_id,user_name, user_email from user_inf');
+    results.forEach((row) {
+      print('ID:${row[0]},Name: ${row[1]}, E-mail: ${row[2]}');
+      userlist.add('"${row[0]}","${row[1]}","${row[2]}"');
+      //userlist.add(['"${row[0]}"','"${row[1]}"']);
+     // userlist.add ({'"ID"':'"${row[0]}"','"Name"':'"${row[1]}"','"email"':'"${row[2]}"'});
+      //IDlist.add('"${row[0]}"');
+      //Namelist.add('"${row[1]}"');
+    });
+  // var query = await pool.prepare('insert into user_inf (user_name,user_email) values (?,?)');
+   //var result = await query.execute(['SXJ', 'SXJ@163.com']);
   request.response.headers
     ..add("Access-Control-Allow-Origin", "*")
     ..add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
     ..add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write('[\n "portmanteau", "fantabulous", "spork", "smog",\n "spanglish", "gerrymander", "turducken", "stagflation",\n "bromance", "freeware", "oxbridge", "palimony", "netiquette",\n "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"\n]\n')
+      ..write(userlist)
+     // ..write('[\n "portmanteau", "fantabulous", "spork", "smog",\n "spanglish", "gerrymander", "turducken", "stagflation",\n "bromance", "freeware", "oxbridge", "palimony", "netiquette",\n "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"\n]\n')
       ..close();
-  }
+
 }
-void addCorsHeaders(HttpResponse res) {
-  res.headers.add("Access-Control-Allow-Origin", "*");
-  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 }
--- web/main.dart
+++ web/main.dart
@@ -3,35 +3,92 @@
 // All rights reserved. Use of this source code 
 // is governed by a BSD-style license that can be 
 // found in the LICENSE file.
-
+//几个还存在的问题和尝试
+//查过注释的文档，各种方式都试过，在服务器的dart文件里还使不能写中文，在客户端的dart里可以。
+//点击一次按钮或者刷新一次127.0.0.1：8080，数据就会翻倍增长，没有查到是什么问题。
+//尝试传入JSON数组[{"ID":"101","NAME":"ZYJ",“EMAIL":"ZYJ@163.COM"},{"ID":"102","NAME":"SYN",“EMAIL":"SYN@163.COM"}]
+//或二维数组[["101","ZYJ","ZYJ@163.COM"]["101","ZYJ","ZYJ@163.COM"]]，输出的是单个字符，decode、encode都试过，未成功。
+//关于用dart将数据写入数据库：
+//var query = await pool.prepare('insert into user_inf (user_name,user_email) values (?,?)');
+// （仅有这条语句时无法在数据库中添加纪录，也不报错，加上下面 query.execute语句后可以添加纪录，但纪录中的数据却是这一条中的，还会添加两条相同的纪录。）
+//var result = await query.execute(['SXJ', 'SXJ@163.com']);
+// （若上条语句values后改为（？，？），则添加的纪录为这条语句中的数据。但依旧出现两条相同数据。）
 import 'dart:html';
 import 'dart:convert';
 
-var wordList;
+var IDList;
+var nameList;
+var EmailList;
 
 void main() {
-  querySelector('#getWords').onClick.listen(makeRequest);
-  wordList = querySelector('#wordList');
+  querySelector('#getID').onClick.listen(makeRequest1);
+  IDList = querySelector('#IDList');
+  querySelector('#getName').onClick.listen(makeRequest2);
+  nameList = querySelector('#nameList');
+  querySelector('#getEmail').onClick.listen(makeRequest3);
+  EmailList = querySelector('#EmailList');
 }
 
-void makeRequest(Event e) {
+void makeRequest1(Event e) {
   var path = 'http://127.0.0.1:8080';
   var httpRequest = new HttpRequest();
   httpRequest
     ..open('GET', path)
-    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..onLoadEnd.listen((e) => requestComplete1(httpRequest))
+    ..send('');
+
+}
+void makeRequest2(Event e) {
+  var path = 'http://127.0.0.1:8080';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete2(httpRequest))
+    ..send('');
+
+}
+void makeRequest3(Event e) {
+  var path = 'http://127.0.0.1:8080';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete3(httpRequest))
     ..send('');
 
 }
 
-requestComplete(HttpRequest request) {
+
+requestComplete1(HttpRequest request) {
   if (request.status == 200) {
-    List<String> portmanteaux = JSON.decode(request.responseText);
-    for (int i = 0; i < portmanteaux.length; i++) {
-      wordList.children.add(new LIElement()..text = portmanteaux[i]);
-    }
-  } else {
-    wordList.children.add(new LIElement()
+    List<String> portmanteaux =JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i=i+3)
+    {IDList.children.add(new LIElement()..text = portmanteaux[i]);}}
+   else {
+      IDList.children.add(new LIElement()
+     ..text = 'Request failed, status=${request.status}');
+   }
+}
+
+
+requestComplete2(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux =JSON.decode(request.responseText);
+    for (int i = 1; i < portmanteaux.length; i=i+3)
+    {nameList.children.add(new LIElement()..text = portmanteaux[i]);}}
+  else {
+    nameList.children.add(new LIElement()
       ..text = 'Request failed, status=${request.status}');
   }
 }
+
+requestComplete3(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux =JSON.decode(request.responseText);
+    for (int i = 2; i < portmanteaux.length; i=i+3)
+    {EmailList.children.add(new LIElement()..text = portmanteaux[i]);}}
+  else {
+    EmailList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
+  }
+}
+
