6f5df946c07016956e5481f7d932431fed589f58
megumii <854613989@qq.com>
2015-10-30 22:20:01 +0800

    exercise_4
    连接数据库（可以在网页中显示出数据了）

--- bin/main.dart
+++ bin/main.dart
@@ -1,34 +1,43 @@
 import 'dart:io';
-import 'dart:async';
-import 'dart:convert';
 import 'package:sqljocky/sqljocky.dart';
+
+List result=new List();
+int i=0;
 
 main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
+  getData();
 
   await for (var request in server) {
-    var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340212', max: 5);
-    var results = await pool.query('select * from user_gyt');
-    var ResultList = new List();
-    results.forEach((row) {
-      print('Name: ${row[0]}, Number: ${row[1]}, Email: ${row[2]}');
-      ResultList.add('Name: ${row[0]}, Number: ${row[1]}, Email: ${row[2]}');
-    });
-
-    addCorsHeaders(request.response);
+    var res= request.response;
+    addCorsHeaders(res);
     request.response
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write(ResultList)
-      /*..write('''[
+      ..write('[\n');
+      for(int j=0;j<result.length;j++)
+      res.write('"${result[j]}');
+      res.write(']');
+/*..write('''[
       "portmanteau", "fantabulous", "spork", "smog",
       "spanglish", "gerrymander", "turducken", "stagflation",
       "bromance", "freeware", "oxbridge", "palimony", "netiquette",
        "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
        ]''')
        */
-      ..close();
+      res.close();
   }
+}
+
+getData()async{
+  var pool=new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340212', max: 5);
+  var results = await pool.query('select * from user_gyt');
+  return results.forEach((row) {
+    print('Name: ${row[0]}, Number: ${row[1]}, Email: ${row[2]}');
+    result.add('Name: ${row[0]}, Number: ${row[1]}, Email: ${row[2]}');
+    result.add('\n');
+    i++;
+  });
 }
 
 void addCorsHeaders(HttpResponse response) {
