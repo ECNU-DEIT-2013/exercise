2689ed749b76929c56c3e557d18f5c1ea765551f
lavenderlyj <498926778@qq.com>
2015-10-30 19:06:15 +0800

    homework-4
    完成服务端连接数据库，并把数据库中的数据调用出来，并通过客户端体现出来

--- change web/index.dart
+++ change web/index.dart
@@ -1,19 +1,9 @@
 import 'dart:html';
 import 'dart:math' as math;
-
-InputElement toDoInput;
-UListElement toDoList;
-
 void main() {
-  toDoInput=querySelector('#to-do-input2');
-  toDoList=querySelector('#to-do-list');
-  toDoInput.onChange.listen(addToDoItem);
-  //querySelector('#sample_text_id')
-    //..text = '='
-   // ..onClick.listen(addToDoItem);
-  querySelector('#clear-id')
-    ..text = 'clear'
-    ..onClick.listen(clearItem);
+  querySelector('#sample_text_id')
+    ..text = '='
+    ..onClick.listen(addNumber);
 }
 
 void addNumber(MouseEvent event)
@@ -29,22 +19,4 @@ void addNumber(MouseEvent event)
   for(i=A;i<=B;i++)
   sum=sum+i;
 querySelector('#sample_text').text=sum.toString();
-}
-
-void addToDoItem(Event e){
-  var newToDo=new LIElement();
-  var A=int.parse(document.getElementById("to-do-input1").value);
-  var B=int.parse(toDoInput.value);
-  var sum=0;
-  sum=A+B;
-  newToDo.text=sum.toString();
-  toDoList.children.add(newToDo);
-  querySelector('#sample_text').text=sum.toString();
-}
-
-void clearItem(MouseEvent event){
-  document.getElementById("to-do-input1").value='';
-  toDoInput.value='';
-  querySelector('#sample_text').text='';
-  //toDoList.children.clear();
 }
--- web/bin/main.dart
+++ web/bin/main.dart
@@ -1,28 +1,34 @@
 import 'dart:io';
-
+import 'package:sqljocky/sqljocky.dart';
+List dataemail = new List();
 main() async {
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
   print("Serving at ${server.address}:${server.port}");
-
+  getData();
 
   await for (var request in server) {
     HttpResponse res = request.response;
     addCorsHeaders(res);
     res
-
       ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write("""[
-  "portmanteau", "fantabulous", "spork", "smog",
-  "spanglish", "gerrymander", "turducken", "stagflation",
-  "bromance", "freeware", "oxbridge", "palimony", "netiquette",
-  "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
-]""")
+      ..write(dataemail)
       ..close();
   }
 }
 
+getData() async{
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340248', max: 5);
+  var results = await pool.query('select * from student;');
+  results.forEach((row) {
+    print('number: ${row.number}, name: ${row.name}, gender:${row.gender}, age: ${row.age},department: ${row.department}');
+    dataemail.add('"${row.number} ${row.name}  ${row.gender} ${row.age} ${row.department}"');
+  });
+  print(dataemail);
+}
+
+
 void addCorsHeaders(HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*");
-  res.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.headers.add("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
