a9422c99131e9f02f480deee6d17645431096368
Dailulucy <dailulucy@163.com>
2015-10-31 23:16:21 +0800

    exercise_4
    第4次的作业（感觉好难啊T T):
    连接服务器，读取数据库中的数据，将数据转换为json格式，并将上述json数据提供给客户端应用，动态控制html，显示获得的数据。

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,17 @@
 import'dart:async';
 import 'dart:io';
+import 'package:sqljocky/sqljocky.dart';
+
 main() async {
+  List a = new List();
+  var pool = new ConnectionPool(host: '52.8.67.180',port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340247', max: 5);
+  var results = await pool.query("SELECT * FROM mystu");
+    results.forEach((row) {
+      print('${row.stuName},${row.stuID},${row.stuMail},${row.stuAge}');
+      a.add('"${row.stuName},${row.stuID},${row.stuMail},${row.stuAge}"');
+
+  });
+
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4,8080);
   print("Serving at ${server.address}:${server.port}");
 
@@ -11,12 +22,7 @@ main() async {
       ..add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       request.response
         ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-        ..write('''[
-  "portmanteau", "fantabulous", "spork", "smog",
-  "spanglish", "gerrymander", "turducken", "stagflation",
-  "bromance", "freeware", "oxbridge", "palimony", "netiquette",
-  "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
-]''')
+        ..write(a)
         ..close();
 
 }
--- web/main.dart
+++ web/main.dart
@@ -8,7 +8,43 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 
+
 var wordList;
+
+//void main(List<String> arguments) {
+  //创建一个连接池，host：连接地址，port：端口，user：用户名，password：密码，db：数据库名，max：最大并发数
+  //var pool = new ConnectionPool(host: '52.8.67.180',port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340247', max: 5);
+  //pool.query("SELECT * FROM users").then((results) {
+   // results.forEach((row) {
+    //  print('${row.stuName},${row.stuID},${row.stuMail},${row.stuAge}');
+    //});
+ // });
+ // querySelector('#getWords').onClick.listen(makeRequest);
+ // wordList = querySelector('#wordList');
+//}
+
+
+
+/*Future makeRequest(Event e) async {
+  request = new HttpRequest();
+
+
+  var url = 'http://127.0.0.1:8080';
+  request.open('GET', url);
+  request.send();
+}
+
+processString(String jsonString) {
+  List<String> portmanteaux = JSON.decode(jsonString);
+  for (int i = 0; i < portmanteaux.length; i++) {
+    wordList.children.add(new LIElement()..text = portmanteaux[i]);
+  }
+}
+
+handleError(Object error) {
+  wordList.children.add(new LIElement()..text = 'Request failed.$Status');
+}
+被隐掉的部分是我之前查文档和在各种教程里搜到的关于链接数据库的代码 粘上去各种组合还是不太对 保留了努力的过程*/
 
 void main() {
   querySelector('#getWords').onClick.listen(makeRequest);
