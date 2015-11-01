23873dc5ffc3326ccf2eba3b6b46189f02120966
zhangyiwei123 <491200649@qq.com>
2015-11-01 21:00:38 +0800

    10130340211
    12

---
+++ web/main.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+main() async {
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+    request.response
+      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+      ..write('''[
+  "portmanteau", "fantabulous", "spork", "smog",
+  "spanglish", "gerrymander", "turducken", "stagflation",
+  "bromance", "freeware", "oxbridge", "palimony", "netiquette",
+  "brunch", "blog", "chortle", "Hassenpfeffer", "Schnitzelbank"
+]''')
+      ..close();
+  }
+}
+
+
