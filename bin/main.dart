6b443615765a62962e4ff9f9eb832117007d38ba
meflyup <meflyup@gmail.com>
2015-11-02 16:48:08 +0800

    没有运行验证？
    // Uncaught Error: Error 1146 (42S02): Table
    'stu_10130340243.stu_10130340243' doesn't exist
    // select中要去的是表，已经不再是stu_10130340243数据库了。你没有运行验证就提交？
    查看main.dart 20,21行

--- bin/main.dart
+++ bin/main.dart
@@ -15,7 +15,10 @@ main() async {
 }
 
 getData() async{
+
   var pool=new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340243', max: 5);
+// Uncaught Error: Error 1146 (42S02): Table 'stu_10130340243.stu_10130340243' doesn't exist
+// select中要去的是表，已经不再是stu_10130340243数据库了。你没有运行验证就提交？
   var results = await pool.query('select * from stu_10130340243');
   results.forEach((row) {
     print('name: ${row[0]}, NO.: ${row[1]},telephone number:${row[2]},E-mail:${row[3]}');
