d4223c85e1b4909b498a43d3e7a566f064088abd
meflyup <meflyup@gmail.com>
2015-11-25 08:44:35 +0800

    rest_frame example
    rest_frame example

--- untitled/bin/server.dart
+++ untitled/bin/server.dart
@@ -5,33 +5,47 @@ import 'dart:async';
 void addCorsHeaders(HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.headers.add("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
 }
 
-main() async {
-  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8008);
-  print("Serving at ${server.address}:${server.port}");
+main()  {
+  Router router = new Router();
+  router.get(getStock, "/stock");
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8008)
+      .then((HttpServer server) {
+    server.listen((HttpRequest request) {
+      router.route(request);
 
+    });
+  });
+}
 
+String getStock() {
+  return "rest_test ：) get stock";
+  //你可以在这里添加各种数据操作，然后以json返回
+}
 
-  await for (var request in server) {
+@RestResource("/bond") /* await for (var request in server) {
     addCorsHeaders(request.response);
-   // handleRequest(request);
+    // handleRequest(request);
     //stockPost(request);
     Router router1 = new Router();
-    router1.post(stockPost, "/stock");//因为在做尝试，但是 不是应该这样写了就可以用吗？？ps：直接用浏览器访问server的地址的话，这个httprequest的method是什么啊？
+    router1.post(stockPost,
+        "/stock"); //因为在做尝试，但是 不是应该这样写了就可以用吗？？ps：直接用浏览器访问server的地址的话，这个httprequest的method是什么啊？
     Router childrouter = router1.child("/bond");
     print('Server is OK');
-  }
-}
+  }*/
+
 
 void handleRequest(HttpRequest request) {
   try {
-    if(request.method == 'POST'){
+    if (request.method == 'POST') {
       handlePost(request);
     }
-    else{
-      request.response..statusCode = HttpStatus.METHOD_NOT_ALLOWED
+    else {
+      request.response
+        ..statusCode = HttpStatus.METHOD_NOT_ALLOWED
         ..write('Unsupported request: ${request.method}.')
         ..close();
     }
@@ -41,13 +55,13 @@ void handleRequest(HttpRequest request) {
   print('Request handled.');
 }
 
-void handlePost(HttpRequest request){
+void handlePost(HttpRequest request) {
 
 }
 
-void stockPost(HttpRequest request){
+void stockPost(HttpRequest request) {
   request.response
     ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
     ..write('test')
     ..close();
-}//不知道怎么写就开始乱写了= =
+} //不知道怎么写就开始乱写了= =
